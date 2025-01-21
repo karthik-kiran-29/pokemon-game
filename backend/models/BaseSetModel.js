a6998a9a8716d8b5578adeaf0a4f53e897a23c87
karthik-kiran-29 <karthikkiran29@gmail.com>
2025-01-21 20:20:29 +0530

    BaseSetModel.js added

---
+++ backend/models/BaseSetModel.js
@@ -0,0 +1,33 @@
+import mongoose from "mongoose";
+
+const baseSetSchema = new mongoose.Schema(
+ {
+    baseId: { type: String, required: true, unique: true },  // e.g., "bw10"
+    name: { type: String, required: true },                  // e.g., "Plasma Blast"
+    series: { type: String, required: true, index: true },   // e.g., "Black & White"
+    printedTotal: { type: Number, required: true },
+    total: { type: Number, required: true },
+    
+    legalities: {
+      unlimited: String,
+      expanded: String
+    },
+    
+    ptcgoCode: String,                                       // e.g., "PLB"
+    releaseDate: Date,
+    updatedAt: Date,
+    
+    images: {
+      symbol: String,
+      logo: String
+    },
+    
+    // Metadata for performance optimization
+    popularityScore: { type: Number, default: 0 },
+    activeInRotation: { type: Boolean, default: true }
+  });
+
+  const baseSetModel = mongoose.model('BaseSet',baseSetSchema);
+
+  export {baseSetModel};
+
