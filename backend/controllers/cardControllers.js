66865d4299ff4a937bea718f8bd5b606953aafe1
karthik-kiran-29 <karthikkiran29@gmail.com>
2025-01-23 00:15:28 +0530

    Added all cards query not complete

--- backend/controllers/cardControllers.js
+++ backend/controllers/cardControllers.js
@@ -4,11 +4,29 @@ const getSingleCard = async (req,res) =>{
     const cardId = req.params.cardId;
     try {
         const result = await CardModel.find({"_id":cardId});
-        res.json({new:"success",result})
+        res.json({result:"success",result})
     } catch (error) {
-        res.json({new:"failed",error: error.message})
+        res.json({result:"failed",error: error.message})
     }
 
 }
 
-export {getSingleCard};
+
+// implementing pagination using limit and skip
+const getAllCards = async (req,res) =>{
+    const name = req.query.name?req.query.name:{};
+    const types = req.query.types?req.query.types:{};
+    const rarity = req.query.rarity?req.query.rarity:{};
+    const baseSet = req.query.baseSet?req.query.baseSet:{};
+    const page = req.query.page?req.query.baseSet:1;
+    
+    try {
+        const result = await CardModel.find({name:{$regex:name,$options:'i'}},{types:types},{rarity:rarity},{baseSet:baseSet}).skip((page -1)*10).limit(10); // needs some changes not their yet
+        res.json({result:"success",result})
+    } catch (error) {
+        res.json({result:"failed",error: error.message})
+    }
+
+}
+
+export {getSingleCard,getAllCards};
--- backend/routes/CardRoute.js
+++ backend/routes/CardRoute.js
@@ -1,11 +1,13 @@
 import express from 'express';
-import { getSingleCard } from '../controllers/cardControllers.js';
+import { getAllCards, getSingleCard } from '../controllers/cardControllers.js';
 
 
 const router = express.Router();
 
-//router.route('/products').get(getProducts)
+router.route('/cards').get(getAllCards)
 
 router.route('/cards/:cardId').get(getSingleCard);
+
+
 
 export default router;
