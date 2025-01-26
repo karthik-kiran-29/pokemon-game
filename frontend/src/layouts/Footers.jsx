6a49804ec7bc3f64a7e964cf823ac778d9fa64a2
karthik-kiran-29 <karthikkiran29@gmail.com>
2025-01-26 18:21:24 +0530

    Added footer

---
+++ frontend/src/layouts/Footers.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Footers = ()=>{
+
+    return(
+        <footer className="py-4">
+                <p className="text-center text-slate-950">
+                Made by Karthik Kiran with 🖤
+                </p>
+            </footer>
+    )
+}
+
+export default Footers;
